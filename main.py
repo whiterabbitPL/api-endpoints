efddee59c2d4effdb7ed6625252bd9ac8a5d5fdc
Ubuntu <ubuntu@vps-52e0d507.vps.ovh.net>
2025-11-28 20:34:33 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Response
+
+def register_routes(app):
+
+    @app.route('/api/test', methods=['GET'])
+    def test_api():
+        html = """
+            <div class='api-form-01'>
+            <h3>Hello world</h3>
+            <p>It is a test API result</p>
+            </div>
+        """
+        return Response(html, mimetype='text/html')
